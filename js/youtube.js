e0f209f13e5bf5276769162f16cd9f49c18a1f08
sean-126 <sungsihoon@gmail.com>
2023-01-19 22:24:31 +0900

    0119 내용 업데이트

---
+++ js/youtube.js
@@ -0,0 +1,25 @@
+// 2. This code loads the IFrame Player API code asynchronously.
+var tag = document.createElement('script');
+
+tag.src = "https://www.youtube.com/iframe_api";
+var firstScriptTag = document.getElementsByTagName('script')[0];
+firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
+
+// 3. This function creates an <iframe> (and YouTube player)
+//    after the API code downloads.
+function onYouTubeIframeAPIReady() {
+  // <div id="player"></div>
+  new YT.Player('player', {
+    videoId: 'An6LvWQuj_8',
+    playerVars: {
+      autoplay: true,
+      loop: true,
+      playlist: 'An6LvWQuj_8'
+    },
+    events: {
+      onReady: function (event) {
+        event.target.mute() //유튜브 영상 음소거 재생, youtube에서는 음소거 기능 제공하지 않아서
+      }
+    }
+  });
+}
